056775f19ef5e9775d5b9166109e5dcdb0f10bde
Kaspar <kasparme@gmail.com>
2022-01-29 11:17:27 +0100

    First

---
+++ application/src/main/java/arraysStrings/slidingWindow/longestSubstringWithoutRepeatingCharacters3/bruteForce/BruteForce.java
@@ -0,0 +1,14 @@
+package arraysStrings.slidingWindow.longestSubstringWithoutRepeatingCharacters3.bruteForce;
+
+public class BruteForce {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println("Hello"+solution.lengthOfLongestSubstring("qq"));
+    }
+}
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        return 2;
+    }
+}
---
+++ application/src/main/java/arraysStrings/slidingWindow/longestSubstringWithoutRepeatingCharacters3/optimal/Optimal.java
@@ -0,0 +1,18 @@
+package arraysStrings.slidingWindow.longestSubstringWithoutRepeatingCharacters3.optimal;
+
+public class Optimal {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println("Hello"+solution.lengthOfLongestSubstring("qq"));
+    }
+}
+
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        return 1;
+    }
+}
+
+
