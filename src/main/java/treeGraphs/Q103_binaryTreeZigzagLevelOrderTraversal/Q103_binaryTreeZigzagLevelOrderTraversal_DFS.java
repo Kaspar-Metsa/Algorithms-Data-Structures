45df8f4c1478212859e21f6e0a2ee67a8a499354
Kaspar Metsa <kaspar.metsa@axual.com>
2022-05-01 06:13:31 +0200

    Refactor

--- src/main/java/treeGraphs/Q103_binaryTreeZigzagLevelOrderTraversal/Q103_binaryTreeZigzagLevelOrderTraversal_DFS.java
+++ src/main/java/treeGraphs/Q103_binaryTreeZigzagLevelOrderTraversal/Q103_binaryTreeZigzagLevelOrderTraversal_DFS.java
@@ -8,27 +8,23 @@ import java.util.List;
 
 public class Q103_binaryTreeZigzagLevelOrderTraversal_DFS {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> sol = new LinkedList<>();
-        traverse(root, sol, 0);
-        return sol;
+        List<List<Integer>> ans = new ArrayList<>();
+        traverse(root, ans, 0);
+        return ans;
     }
 
-    private void traverse(TreeNode cur, List<List<Integer>> sol, int lvl) {
+    private void traverse(TreeNode cur, List<List<Integer>> ans, int lvl) {
         if (cur == null) return;
+        if (ans.size() == lvl) ans.add(new LinkedList<>()); // This can't be ArrayList, because we use addFirst() later, because adding to start of ArrayList is O(n)
+        LinkedList<Integer> currentLevel = (LinkedList<Integer>) ans.get(lvl); // This casting is necessary because signature from exercise requires returning List<List<Integer>>. In a perfect world we would use ArrayDeque
 
-        if (sol.size() <= lvl) {
-            LinkedList<Integer> newLvl = new LinkedList<>();
-            sol.add(newLvl);
-        }
-
-        LinkedList<Integer> lvlList = (LinkedList<Integer>) sol.get(lvl); // This casting is necessary because signature from exercise requires returning List<List<Integer>>. In a perfect world we would use ArrayDeque
         if (lvl % 2 == 0) {
-            lvlList.addLast(cur.val); // equivalent to add
+            currentLevel.addLast(cur.val); // equivalent to add
         } else {
-            lvlList.addFirst(cur.val); //Because lvlColl.add(0, cur.val); will move every element forward by 1 index. We cast because addFirst() is a Deque interface method, not List interface method
+            currentLevel.addFirst(cur.val); //Because lvlColl.add(0, cur.val); will move every element forward by 1 index. We cast because addFirst() is a Deque interface method, not List interface method
         }
 
-        traverse(cur.left, sol, lvl + 1);
-        traverse(cur.right, sol, lvl + 1);
+        traverse(cur.left, ans, lvl + 1);
+        traverse(cur.right, ans, lvl + 1);
     }
 }
